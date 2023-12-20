ef9ae614b957470aeb93ad9b3e2d90e8a7a48a3c
nandabi <valerie.nandabi@student.moringaschool.com>
2023-12-20 21:37:59 +0300

    commit message

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+
+function returnsANamedFunction() {
+  return function namedFunction(){
+    console.log('namedFunction')
+
+  }
+}
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log('anonymousFunction')
+  }
+}
